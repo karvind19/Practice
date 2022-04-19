1d8a4f0e2869dcf3040da60c184d8715d15ddef5
Arvind Kumar <KumarArvi@india.rapidigm.com>
2022-04-19 13:39:30 +0530

    Adding all the files along with project

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,65 @@
+﻿using BookAPI.BookRepository;
+using BookAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Book>> GetBooks(int Id)
+        {
+            return await _bookRepository.Get(Id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>>PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            await _bookRepository.Update(book);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+            if (bookToDelete == null)
+                return NotFound();
+
+            await _bookRepository.Delete(bookToDelete.Id);
+            return NoContent();
+        }
+    }
+}
